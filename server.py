d93fafdf06410e5e15dc0bfd9e2f55f3190b6491
BuildTools <unconfigured@null.spigotmc.org>
2021-08-07 16:24:57 -0400

    github pages routing 2

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ import crimes
 def indexfunction():
   return bottle.static_file('index.html', root='')
 
-@bottle.route('/ericv105/map.js')
+@bottle.route('/Crime-Incidents-in-Buffalo/map.js')
 def mapfunction():
   return bottle.static_file('map.js', root='')
 
-@bottle.route('/ericv105/crime')
+@bottle.route('/Crime-Incidents-in-Buffalo/crime')
 def get_tickets():
   return crimes.get_ticket_data("https://data.buffalony.gov/resource/d6g9-xbgu.json")
 
