d3bab542bee71830f2952b094fe4b606cdcea4a6
BuildTools <unconfigured@null.spigotmc.org>
2021-08-07 16:24:25 -0400

    gh pages routing

--- server.py
+++ server.py
@@ -5,11 +5,11 @@ import crimes
 def indexfunction():
   return bottle.static_file('index.html', root='')
 
-@bottle.route('/map.js')
+@bottle.route('/ericv105/map.js')
 def mapfunction():
   return bottle.static_file('map.js', root='')
 
-@bottle.route('/crime')
+@bottle.route('/ericv105/crime')
 def get_tickets():
   return crimes.get_ticket_data("https://data.buffalony.gov/resource/d6g9-xbgu.json")
 
